f342133f123b984f244558f85ca982f0ea09d562
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-06-22 14:14:12 +0100

    Initial Commit

---
+++ swiftfulb/swiftfulbApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulbApp.swift
+//  swiftfulb
+//
+//  Created by Fiyi Oladimeji on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
