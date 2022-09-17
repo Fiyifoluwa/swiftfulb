16088ffa7c025b96ab7f41140de89550a3442d53
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-09-17 23:49:43 +0100

    state learning

---
+++ swiftfulb/ExtractionLearning.swift
@@ -0,0 +1,20 @@
+//
+//  ExtractionLearning.swift
+//  swiftfulb
+//
+//  Created by Fiyi Oladimeji on 17/09/2022.
+//
+
+import SwiftUI
+
+struct ExtractionLearning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExtractionLearning_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractionLearning()
+    }
+}
---
+++ swiftfulb/StateLearning.swift
@@ -0,0 +1,56 @@
+//
+//  StateLearning.swift
+//  swiftfulb
+//
+//  Created by Fiyi Oladimeji on 17/09/2022.
+//
+
+import SwiftUI
+
+struct StateLearning: View {
+    
+    @State var bgColor: Color = Color.green
+    @State var myTitle: String = "my title"
+    @State var count: Int = 0
+    @State var space: CGFloat = 20
+    
+    var body: some View {
+        ZStack{
+//            background
+            bgColor
+                .edgesIgnoringSafeArea(.all )
+            
+//            content / foreground
+            VStack(spacing: 20){
+                Text(myTitle)
+                    .font(.title)
+                Text("Count: \(count)")
+                    .font(.headline)
+                    .underline()
+                
+                
+                HStack(spacing: space){
+                    Button("Button 1".uppercased()) {
+                        bgColor = .red
+                        myTitle = "Button 1 was pressed. E wan piss"
+                        count += 1
+                        space += 5
+                    }
+                    
+                    Button("Button 2".uppercased()) {
+                        bgColor = .purple
+                        myTitle = "Button 2  was pressed. E enter like bread"
+                        count -= 1
+                        space -= 5
+                    }
+                }
+            }
+            .foregroundColor(.white)
+        }    }
+}
+
+struct StateLearning_Previews: PreviewProvider {
+    static var previews: some View {
+        StateLearning()
+    }
+}
