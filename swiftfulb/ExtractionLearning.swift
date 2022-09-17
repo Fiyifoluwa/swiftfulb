ffd83f0f68f2c00c2220bae1efb4923777b1afe7
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-09-18 00:12:24 +0100

    function extraction bit

--- swiftfulb/ExtractionLearning.swift
+++ swiftfulb/ExtractionLearning.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ExtractionLearning: View {
+    
+    @State var bgColor: Color = Color.pink
+    @State var titleColor: Color = Color.black
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+//            background
+            bgColor
+                .edgesIgnoringSafeArea(.all)
+            
+//            foreground or content layer
+            contentArea
+        }
+    }
+    
+    var contentArea: some View {
+        VStack{
+            Text("Title")
+                .font(.largeTitle)
+                .foregroundColor(titleColor)
+            
+            Button {
+                    buttonPressed()
+            } label: {
+                Text("press me".uppercased())
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(10)
+            }
+        }
+    }
+    
+    func buttonPressed(){
+//        bgColor = .yellow
+        
+        if bgColor == .pink {
+            bgColor = .yellow
+            titleColor = .black
+            
+        } else if bgColor == .yellow {
+            bgColor = .pink
+            titleColor = .white
+        }
     }
 }
 
