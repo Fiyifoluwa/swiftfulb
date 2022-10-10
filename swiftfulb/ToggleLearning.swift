b3317a27dcf6be3451becbc74f560f0513a0bff2
Fiyifoluwa <fiyifoluwa@outlook.com>
2022-10-10 15:07:42 +0100

    toggle in swift ui

---
+++ swiftfulb/ToggleLearning.swift
@@ -0,0 +1,35 @@
+//
+//  ToggleLearning.swift
+//  swiftfulb
+//
+//  Created by Fiyi Oladimeji on 10/10/2022.
+//
+
+import SwiftUI
+
+struct ToggleLearning: View {
+    @State var toggleIsOn: Bool = false
+    
+    
+    var body: some View {
+        VStack {
+            HStack{
+            Text("Status:")
+                Text(toggleIsOn ? "Online" : "Offline")
+            }
+            Toggle(isOn: $toggleIsOn) {
+                Text("Change status")
+            }
+            .toggleStyle(SwitchToggleStyle(tint: .red))
+            
+            Spacer()
+        }
+        .padding(.horizontal, 100)
+    }
+}
+
+struct ToggleLearning_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleLearning()
+    }
+}
